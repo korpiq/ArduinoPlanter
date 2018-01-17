d0aedb5b467a2c5534ce513b6fe0a1eaf527e375
Kalle Hallivuori <kalle.hallivuori@futurice.com>
2018-01-17 22:14:31 +0200

    Add project files.

---
+++ ArduinoPlanter/ArduinoPlanter.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduinoPlanter.ino
+ Created:	1/17/2018 10:14:21 PM
+ Author:	kalle
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
