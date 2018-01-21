9a57b5e7a5daa4743edc8b044c791392bf69263b
Kalle Hallivuori <kalle.hallivuori@futurice.com>
2018-01-21 15:09:03 +0200

    Explicitly mark all times in configuration as unsigned long

--- ArduinoPlanter/Configuration.c
+++ ArduinoPlanter/Configuration.c
@@ -12,17 +12,17 @@ arduino_planter_configuration_t default_configuration = {
 	.lamp_pin = 2,
 	.pump_pin = 3,
 
-	.lamp_cycle_time = 86400000L, // every 24 hours
-	.lamp_delay_time = 28800000L, // activate after 8 hours
-	.lamp_active_time = 57600000L, // show light for 16 hours
+	.lamp_cycle_time = 86400000ul, // every 24 hours
+	.lamp_delay_time = 28800000ul, // activate after 8 hours
+	.lamp_active_time = 57600000ul, // show light for 16 hours
 
-	.pump_cycle_time = 21600000L, // every 6 hours
-	.pump_delay_time = 0L, // activate at beginning of cycle
-	.pump_active_time = 600000L, // run for 10 minutes
+	.pump_cycle_time = 21600000ul, // every 6 hours
+	.pump_delay_time = 0ul, // activate at beginning of cycle
+	.pump_active_time = 600000ul, // run for 10 minutes
 
-	.air_read_interval = 2000L, // every 2 seconds
-	.report_interval = 2000L, // every 2 seconds
-	.communication_timeout = 3600000L, // 1 hour
+	.air_read_interval = 2000ul, // every 2 seconds
+	.report_interval = 2000ul, // every 2 seconds
+	.communication_timeout = 3600000ul, // 1 hour
 
 	.serial_port_speed = 115200,
 	.water_sensor_value_when_wet = 1
