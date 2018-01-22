4f96f6cdfb4c863d4db3fd7da05c79d315d4778d
Kalle Hallivuori <kalle.hallivuori@futurice.com>
2018-01-22 19:50:32 +0200

    Separated production and debug configurations.

--- ArduinoPlanter/Configuration.c
+++ ArduinoPlanter/Configuration.c
@@ -2,7 +2,9 @@
 
 /* Values for planter configuration settings */
 
-arduino_planter_configuration_t default_configuration = {
+#ifndef _DEBUG
+
+arduino_planter_configuration_t default_configuration = { // PRODUCTION configuration
 	.acidity_pin = 0,
 
 	.air_read_pin = 4,
@@ -12,13 +14,41 @@ arduino_planter_configuration_t default_configuration = {
 	.lamp_pin = 2,
 	.pump_pin = 3,
 
-	.lamp_cycle_time = 12000ul, // 86400000ul, // every 24 hours
-	.lamp_delay_time = 2000ul, // 1s, -- 28800000ul, // activate after 8 hours
-	.lamp_active_time = 8000ul, // 57600000ul, // show light for 16 hours
+	.lamp_cycle_time = 86400000ul, // every 24 hours
+	.lamp_delay_time = 5000ul, // activate after 5 seconds
+	.lamp_active_time = 57600000ul, // show light for 16 hours
 
-	.pump_cycle_time = 4000ul, // 21600000ul, // every 6 hours
-	.pump_delay_time = 30000ul, // activate at beginning of cycle
-	.pump_active_time = 1000ul, // 600000ul, // run for 10 minutes
+	.pump_cycle_time = 21600000ul, // every 6 hours
+	.pump_delay_time = 5000ul, // activate after 5 seconds
+	.pump_active_time = 600000ul, // run for 10 minutes at most
+
+	.air_read_interval = 2000ul, // every 2 seconds
+	.report_interval = 60000ul, // every 1 minute
+	.communication_timeout = 3600000ul, // 1 hour
+
+	.serial_port_speed = 115200,
+	.water_sensor_value_when_wet = 1
+};
+
+#else
+
+arduino_planter_configuration_t default_configuration = { // DEBUG configuration
+	.acidity_pin = 0,
+
+	.air_read_pin = 4,
+	.water_bottom_pin = 5,
+	.water_top_pin = 6,
+
+	.lamp_pin = 2,
+	.pump_pin = 3,
+
+	.lamp_cycle_time = 12000ul, // 12 second "day"
+	.lamp_delay_time = 2000ul,
+	.lamp_active_time = 8000ul,
+
+	.pump_cycle_time = 4000ul, // 4 second watering cycle
+	.pump_delay_time = 30000ul,
+	.pump_active_time = 1000ul,
 
 	.air_read_interval = 2000ul, // every 2 seconds
 	.report_interval = 5000ul, // every 5 seconds
@@ -27,3 +57,5 @@ arduino_planter_configuration_t default_configuration = {
 	.serial_port_speed = 115200,
 	.water_sensor_value_when_wet = 1
 };
+
+#endif
