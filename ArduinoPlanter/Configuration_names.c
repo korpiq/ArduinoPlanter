6c86c4f676cebd6f3987e632c5624187e74725c8
Kalle Hallivuori <kalle.hallivuori@futurice.com>
2018-02-04 16:32:43 +0200

    Configuration names defined only once, but eating more memory.

---
+++ ArduinoPlanter/Configuration_names.c
@@ -0,0 +1,23 @@
+
+#include "Configuration.h"
+
+const char * const configuration_setting_names[NUMBER_OF_CONFIGURATION_SETTINGS] =
+{
+	"acidity_pin",
+	"air_read_pin",
+	"water_bottom_pin",
+	"water_top_pin",
+	"lamp_pin",
+	"pump_pin",
+	"lamp_cycle_time",
+	"lamp_active_time",
+	"pump_cycle_time",
+	"pump_active_time",
+	"air_read_interval",
+	"report_interval",
+	"remote_control_timeout",
+	"startup_delay",
+	"serial_port_speed",
+	"water_sensor_value_when_wet",
+	"air_sensor_type"
+};
