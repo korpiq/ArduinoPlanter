dc11d5b903e4337e5de89ce05eb81fa6e205fec9
Kalle Hallivuori <kalle.hallivuori@futurice.com>
2018-04-21 21:29:22 +0300

    Added a unit test for JSON flattening function.

---
+++ AzureFunctionsTest/JsonFlattenTest.cs
@@ -0,0 +1,43 @@
+﻿using AzureFunctions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AzureFunctions
+{
+    [TestClass]
+    public class TestJsonFlatten
+    {
+        PrivateType privateTypeJsonClass = new PrivateType(typeof(JsonFlatten));
+
+        [TestMethod]
+        public void TestFlattenDeeplyNestedJsonOk()
+        {
+            Dictionary<string, object>
+                deepInput = new Dictionary<string, object>(),
+                secondLevelObject = new Dictionary<string, object>(),
+                thirdLevelObject = new Dictionary<string, object>(),
+                expected = new Dictionary<string, object>(),
+                actual = new Dictionary<string, object>();
+
+            string
+                topLevelScalar = "top level trivial value",
+                secondLevelScalar = "second level trivial value",
+                thirdLevelScalar = "third level trivial value";
+
+            thirdLevelObject["thirdLevelScalar"] = thirdLevelScalar;
+            secondLevelObject["thirdLevelObject"] = thirdLevelObject;
+            secondLevelObject["secondLevelScalar"] = secondLevelScalar;
+            deepInput["secondLevelObject"] = secondLevelObject;
+            deepInput["topLevelScalar"] = topLevelScalar;
+
+            expected["topLevelScalar"] = topLevelScalar;
+            expected["secondLevelObject_secondLevelScalar"] = secondLevelScalar;
+            expected["secondLevelObject_thirdLevelObject_thirdLevelScalar"] = thirdLevelScalar;
+
+            object[] parameters = { deepInput, actual, "", "_" };
+            privateTypeJsonClass.InvokeStatic("Flatten", parameters);
+
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+    }
+}
---
+++ AzureFunctionsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AzureFunctionsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AzureFunctionsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("82b532b9-16e5-4b0e-a1f7-c098051857fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
