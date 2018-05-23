c658d767ca051e372e0a1ddde13fe685bbb6d57b
Kalle Hallivuori <kalle.hallivuori@futurice.com>
2018-05-23 20:59:35 +0300

    Fix IothubClient python object self-referencing.

--- RpiPlantation/IothubClient.py
+++ RpiPlantation/IothubClient.py
@@ -21,33 +21,33 @@ class IothubClient(object):
 
     def iothub_client_init(self):
         # prepare iothub client
-        client = IoTHubClient(self.iothub_connection_string, protocol)
+        self.client = IoTHubClient(self.iothub_connection_string, protocol)
         # set the time until a message times out
-        client.set_option("messageTimeout", message_timeout)
-        client.set_option("logtrace", 0)
-        client.set_option("product_info", "rpi-plantation")
-        messages_sent = 0
-        messages_confirmed = 0
-        return client
+        self.client.set_option("messageTimeout", message_timeout)
+        self.client.set_option("logtrace", 0)
+        self.client.set_option("product_info", "rpi-plantation")
+        self.messages_sent = 0
+        self.messages_confirmed = 0
+        return self.client
 
     def get_client(self):
-        if client is None:
-            client = self.iothub_client_init()
-        return client
+        if self.client is None:
+            self.client = self.iothub_client_init()
+        return self.client
 
     def send_confirmation_callback(self, message, result, user_context):
         if result == IoTHubClientResult.OK:
-            messages_confirmed += 1
+            self.messages_confirmed += 1
         else:
              sys.stderr.writeln( "IoTHub send \"%s\" failed: \"%s\"" % (message.message_id, result) )
-             client = None
+             self.client = None
 
     def send(self, message):
         try:
             iothub_message = IoTHubMessage(message)
-            messages_sent += 1
+            self.messages_sent += 1
             iothub_message.message_id = "message_%d" % messages_sent
             self.get_client().send_event_async(iothub_message, send_confirmation_callback, messages_sent)
         except IoTHubError as iothub_error:
              sys.stderr.writeln( "IoTHub error: \"%s\"" % iothub_error )
-             client = None
+             self.client = None
